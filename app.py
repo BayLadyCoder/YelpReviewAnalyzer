1d5d342ba3c7986a309b6f3b497b4a04ac42ed22
BayLadyCoder <rachada_32@hotmail.com>
2019-04-04 01:28:59 -0400

    Added Bootstrap and contact.html

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def hello():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About")
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title="Contact")
 
 if __name__ == '__main__':
     app.run(debug=True)
